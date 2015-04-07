9734f20c4275e34425b4c919277c3f31b2700b1e
Fazendeiro Léo <fazendeiro.leo@gmail.com>
2015-04-07 21:55:41 +0200

    Ajout du web service

---
+++ Web Service/server.php
@@ -0,0 +1,33 @@
+<?php
+ 
+// première étape : désactiver le cache lors de la phase de test
+ini_set("soap.wsdl_cache_enabled", "0");
+ 
+// on indique au serveur à quel fichier de description il est lié
+$serveurSOAP = new SoapServer('HelloYou.wsdl');
+ 
+// ajouter la fonction getHello au serveur
+$serveurSOAP->addFunction('getHello');
+$serveurSOAP->addFunction('getHelloReverse');
+ 
+// lancer le serveur
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+ 
+{
+	$serveurSOAP->handle();
+}
+else
+{
+	echo 'désolé, je ne comprends pas les requêtes GET, veuillez seulement utiliser POST';
+}
+ 
+function getHello($prenom, $nom)
+{
+	return 'Hello ' . $prenom . ' ' . $nom;
+}
+function getHelloReverse($prenom, $nom)
+{
+	return 'Hello ' . $nom . ' ' . $prenom;
+}
+?>
+
